74c3d69ac53d4f7fac9eef2c32b4a556c16116d4
siddy <saidimmaka@gmail.com>
2020-02-07 00:22:08 +0300

    swipe to refresh fruit reccycler

--- app/src/main/java/com/example/myapplication/activity/ListFruits.kt
+++ app/src/main/java/com/example/myapplication/activity/ListFruits.kt
@@ -1,35 +1,46 @@
 package com.example.myapplication.activity
 
-import android.content.DialogInterface
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import android.view.MotionEvent
 import android.view.View
 import android.view.animation.AnimationUtils
-import androidx.appcompat.app.AlertDialog
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.*
-import com.example.myapplication.adapter.ListFruitsAdapter
-import com.example.myapplication.api.ServerApi
 import com.example.myapplication.Model.MatundaResponse
 import com.example.myapplication.Model.Tunda
+import com.example.myapplication.R
+import com.example.myapplication.adapter.ListFruitsAdapter
+import com.example.myapplication.api.ServerApi
+import com.google.android.material.navigation.NavigationView
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.list_fruits_layout.*
+import kotlinx.android.synthetic.main.navigation_drawer_layout.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class ListFruits : AppCompatActivity() {
+
+class ListFruits : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     lateinit var lfAdapter: ListFruitsAdapter
     lateinit var mData: ArrayList<Tunda>
     var refreshTimes = 0
+
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,6 +77,43 @@ class ListFruits : AppCompatActivity() {
             loadFruits()
             itemsswipetorefresh.isRefreshing = false
         }
+
+//        t = ActionBarDrawerToggle(this, nav_drawer, R.string.open, R.string.close)
+//        t!!.isDrawerIndicatorEnabled = true
+//        nav_drawer.addDrawerListener(t!!)
+//        t!!.syncState()
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+//        supportActionBar?.setHomeButtonEnabled(true)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        drawerLayout = findViewById(R.id.nav_drawer)
+        navView = findViewById(R.id.nav_view)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, 0, 0
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.account -> {
+                Toast.makeText(this, "Profile clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.settings -> {
+                Toast.makeText(this, "Messages clicked", Toast.LENGTH_SHORT).show()
+            }
+            R.id.edit_profile -> {
+                Toast.makeText(this, "Friends clicked", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 
     private fun loadFruits(){
