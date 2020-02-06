9e0da6ca75efdf8981aba2815e7fec66fedff702
siddy <saidimmaka@gmail.com>
2020-02-06 15:50:40 +0300

    swipe to refresh fruit reccycler

--- app/src/main/java/com/example/myapplication/activity/ListFruits.kt
+++ app/src/main/java/com/example/myapplication/activity/ListFruits.kt
@@ -1,13 +1,17 @@
 package com.example.myapplication.activity
 
 import android.content.DialogInterface
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
+import android.view.MotionEvent
 import android.view.View
 import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.*
 import com.example.myapplication.adapter.ListFruitsAdapter
 import com.example.myapplication.api.ServerApi
@@ -25,18 +29,43 @@ class ListFruits : AppCompatActivity() {
 
     lateinit var lfAdapter: ListFruitsAdapter
     lateinit var mData: ArrayList<Tunda>
+    var refreshTimes = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.list_fruits_layout)
 
         listFruitscyclerView.layoutManager=LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        listFruitscyclerView.addOnItemTouchListener(
+            object: RecyclerView.OnItemTouchListener {
+                override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {}
+                override fun onInterceptTouchEvent(rv: RecyclerView, e:
+                MotionEvent): Boolean {
+                    if (e.action == MotionEvent.ACTION_DOWN &&
+                        rv.scrollState == RecyclerView.SCROLL_STATE_SETTLING) {
+                        rv.stopScroll()
+                    }
+                    return false
+                }
+                override fun onRequestDisallowInterceptTouchEvent(
+                    disallowIntercept: Boolean) {}
+            })
 
         val resId = R.anim.slide_down
         val animation = AnimationUtils.loadAnimation(this, resId)
         listFruitscyclerView.startAnimation(animation)
 
         loadFruits()
+
+        //** Set the colors of the Pull To Refresh View
+        itemsswipetorefresh.setProgressBackgroundColorSchemeColor(ContextCompat.getColor(this, R.color.colorPrimary))
+        itemsswipetorefresh.setColorSchemeColors(Color.WHITE)
+
+        itemsswipetorefresh.setOnRefreshListener {
+            mData.clear()
+            loadFruits()
+            itemsswipetorefresh.isRefreshing = false
+        }
     }
 
     private fun loadFruits(){
@@ -74,6 +103,8 @@ class ListFruits : AppCompatActivity() {
                             applicationContext,
                             this@ListFruits
                         )
+
+                    lfAdapter.notifyDataSetChanged()
 
                     listFruitscyclerView.adapter = lfAdapter
 
